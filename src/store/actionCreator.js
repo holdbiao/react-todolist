61b9add7b5a781b454f8b3062bb1e0955f36cf8a
holdbiao <1007059411@qq.com>
2019-09-25 18:58:20 +0800

    提交代码

--- src/store/actionCreator.js
+++ src/store/actionCreator.js
@@ -6,7 +6,7 @@ export const initListAction = (data) => ({
 })
 
 export const getTodoList = () => {
-  return (dispatch) => { // 使用后这里可以返回方法,并接收到dispatch
+  return (dispatch) => { // 使用redux-thunk后这里可以返回方法,并接收到dispatch
     // 这里写异步请求
     const data = ['1', '2', '3', '4', '5']
     const action = initListAction(data)
